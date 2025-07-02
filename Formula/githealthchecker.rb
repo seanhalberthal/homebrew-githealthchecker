aadd412ab72327aee06681f9630d3b02408e2029
bssmnt <socuting@gmail.com>
2025-07-02 12:57:37 +0100

    fix: updated formula

--- Formula/githealthchecker.rb
+++ Formula/githealthchecker.rb
@@ -2,13 +2,13 @@
     desc "Comprehensive CLI tool to analyze Git repositories for security, performance, and quality issues"
     homepage "https://github.com/seanhalberthal/githealthchecker"
     url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.0.5/githealthchecker-v1.0.5-darwin-amd64.tar.gz"
-    version "1.0.5"
-    sha256 "ee53006b8de6a15ae640e95f7a5f7daaae95966bc7afd848423f97eed2693b4c"
+    version "1.0.7"
+    sha256 "325af71b49b406ea2424ef11fdc9103f4619d35b4e0748a590dbb705ce3de4b0"
     license "MIT"
 
     on_linux do
       url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.0.5/githealthchecker-v1.0.5-linux-amd64.tar.gz"
-      sha256 "cb8a3611b72e144f9802cfe2c0854fcab04b6191cecabbe506d2f03c4af92e95"
+      sha256 "842f76caf5a4b3b6352ec450db7fbb175ffd402d1b753802055c47afda2dfd61"
     end
 
     def install
