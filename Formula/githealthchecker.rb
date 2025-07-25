7b433faf1a84a7af018b747e929c661bbbd4dcb1
GitHub Action <action@github.com>
2025-07-25 18:52:35 +0000

    feat: update githealthchecker to v1.1.3
      Auto-generated from release v1.1.3
      Source: seanhalberthal/githealthchecker

--- Formula/githealthchecker.rb
+++ Formula/githealthchecker.rb
@@ -1,14 +1,14 @@
 class Githealthchecker < Formula
   desc "Comprehensive CLI tool to analyze Git repositories for security, performance, and quality issues"
   homepage "https://github.com/seanhalberthal/githealthchecker"
-  url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.1.2/githealthchecker-v1.1.2-darwin-amd64.tar.gz"
-  version "1.1.2"
-  sha256 "b8eaa3a77522e708afcd338a895bd352f1a13aa9855948102ce44743e67e27a4"
+  url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.1.3/githealthchecker-v1.1.3-darwin-amd64.tar.gz"
+  version "1.1.3"
+  sha256 "83f9b732bf4a1bddbb3e3ffafbd1309a6018db975deb36e24ac035e2b799fdd9"
   license "MIT"
 
   on_linux do
-    url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.1.2/githealthchecker-v1.1.2-linux-amd64.tar.gz"
-    sha256 "806243a80fd378ea40daee0cd1b3873cbdd483b8c728521cae2ab44d2166e3c3"
+    url "https://github.com/seanhalberthal/githealthchecker/releases/download/v1.1.3/githealthchecker-v1.1.3-linux-amd64.tar.gz"
+    sha256 "a87d5e936281f356719e49c1992819ae40d98e088eb0d7348c21e73981c94eca"
   end
 
   def install
